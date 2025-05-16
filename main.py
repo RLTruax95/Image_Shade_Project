ef6411e88d8dc74902980c09ad0135d97bc224be
Ryan Truax <truax53@live.com>
2025-05-15 20:20:49 -0400

    get image path, load image, convert to a numpy array, and confirm the image is the correct size and has the rgb values

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import numpy as np
+from PIL import Image
+
 def main():
-    print("Hello World")
+    path = input("Enter the absolute image path (no quotations): ")
+    image = Image.open(path)
+    image_array = np.array(image)
+    print(f'Shape: {image_array.shape}')
 
 if __name__ == '__main__':
     main()
